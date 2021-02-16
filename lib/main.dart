dff342380ce8ef303358946865f83dea8ab6317f
Rahul Singh <developer.rahulsingh.001@gmail.com>
2021-02-16 16:53:43 +0530

    fix: minSdkVersion issue.

--- lib/main.dart
+++ lib/main.dart
@@ -24,74 +24,79 @@ import 'package:cointopper/bloc/week_day_history_bloc/week_day_history_bloc.dart
 import 'package:cointopper/bloc/week_day_history_bloc/week_day_history_event.dart';
 import 'package:cointopper/repositories/repo.dart';
 import 'package:cointopper/screens/bottom_tab_navigation.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
-  runApp(MultiBlocProvider(
-    providers: [
-      BlocProvider<DashboardBloc>(
-        create: (context) => DashboardBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadGlobalDataCoin()),
-      ),
-      BlocProvider<CurrencyListBloc>(
-        create: (BuildContext context) => CurrencyListBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadCurrencyList()),
-      ),
-      BlocProvider<TopViewedCoinListBloc>(
-        create: (BuildContext context) => TopViewedCoinListBloc(
-          coinRepository: CoinTopperRepository(),
-        )..add(LoadTopViewedCoinList('INR')),
-      ),
-      BlocProvider<CoinListBloc>(
-        create: (BuildContext context) => CoinListBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadCoinList('INR', 0, 0)),
-      ),
-      BlocProvider<SearchCoinListBloc>(
-        create: (BuildContext context) => SearchCoinListBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadSearchCoinList()),
-      ),
-      BlocProvider<CoinDetailBloc>(
-        create: (BuildContext context) => CoinDetailBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadCoinDetail("", 'USD')),
-      ),
-      BlocProvider<AllHistoryBloc>(
-        create: (BuildContext context) => AllHistoryBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadAllHistory(0)),
-      ),
-      BlocProvider<WeekDayHistoryBloc>(
-        create: (BuildContext context) => WeekDayHistoryBloc(
-          coinTopperRepository: CoinTopperRepository(),
-        )..add(LoadWeekDayHistory(0)),
-      ),
-      BlocProvider<FeaturedNewsBloc>(
-        create: (BuildContext context) => FeaturedNewsBloc(
-          coinRepository: CoinTopperRepository(),
-        )..add(LoadFeaturedNewsList()),
-      ),
-      BlocProvider<NewsListBloc>(
-        create: (BuildContext context) => NewsListBloc(
-          coinRepository: CoinTopperRepository(),
-        )..add(LoadNewsList()),
-      ),
-      BlocProvider<NewsSearchBloc>(
-        create: (BuildContext context) => NewsSearchBloc(
-          coinRepository: CoinTopperRepository(),
-        )..add(LoadNewsSearch("")),
-      ),
-      BlocProvider<NewsDetailsBloc>(
-        create: (BuildContext context) => NewsDetailsBloc(
-          coinRepository: CoinTopperRepository(),
-        )..add(LoadNewsDetails(0)),
-      ),
-    ],
-    child: MyApp(),
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MultiBlocProvider(
+      providers: [
+        BlocProvider<DashboardBloc>(
+          create: (context) => DashboardBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadGlobalDataCoin()),
+        ),
+        BlocProvider<CurrencyListBloc>(
+          create: (BuildContext context) => CurrencyListBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadCurrencyList()),
+        ),
+        BlocProvider<TopViewedCoinListBloc>(
+          create: (BuildContext context) => TopViewedCoinListBloc(
+            coinRepository: CoinTopperRepository(),
+          )..add(LoadTopViewedCoinList('INR')),
+        ),
+        BlocProvider<CoinListBloc>(
+          create: (BuildContext context) => CoinListBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadCoinList('INR', 0, 0)),
+        ),
+        BlocProvider<SearchCoinListBloc>(
+          create: (BuildContext context) => SearchCoinListBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadSearchCoinList()),
+        ),
+        BlocProvider<CoinDetailBloc>(
+          create: (BuildContext context) => CoinDetailBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadCoinDetail("", 'USD')),
+        ),
+        BlocProvider<AllHistoryBloc>(
+          create: (BuildContext context) => AllHistoryBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadAllHistory(0)),
+        ),
+        BlocProvider<WeekDayHistoryBloc>(
+          create: (BuildContext context) => WeekDayHistoryBloc(
+            coinTopperRepository: CoinTopperRepository(),
+          )..add(LoadWeekDayHistory(0)),
+        ),
+        BlocProvider<FeaturedNewsBloc>(
+          create: (BuildContext context) => FeaturedNewsBloc(
+            coinRepository: CoinTopperRepository(),
+          )..add(LoadFeaturedNewsList()),
+        ),
+        BlocProvider<NewsListBloc>(
+          create: (BuildContext context) => NewsListBloc(
+            coinRepository: CoinTopperRepository(),
+          )..add(LoadNewsList()),
+        ),
+        BlocProvider<NewsSearchBloc>(
+          create: (BuildContext context) => NewsSearchBloc(
+            coinRepository: CoinTopperRepository(),
+          )..add(LoadNewsSearch("")),
+        ),
+        BlocProvider<NewsDetailsBloc>(
+          create: (BuildContext context) => NewsDetailsBloc(
+            coinRepository: CoinTopperRepository(),
+          )..add(LoadNewsDetails(0)),
+        ),
+      ],
+      child: MyApp(),
+    ),
   ));
 }
 
@@ -100,6 +105,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
